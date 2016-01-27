5322109a6ba25161d241cdd9ae23c067d27fb97f
Budi Irawan <deerawan@gmail.com>
2016-01-27 21:07:46 +0700

    add two commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,45 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode'; 
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import {window, Selection, Position, ExtensionContext, commands} from 'vscode';
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-whitespace" is now active!'); 
+export function activate(context: ExtensionContext) {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-		// The code you place here will be executed every time your command is executed
+	console.log('Congratulations, your extension "vscode-whitespace" is now active!');
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	var disposable = commands.registerCommand('extension.convertTabsToSpaces', () => {
+		var editor = window.activeTextEditor;
+        var options = editor.options;
+        var document = editor.document;
+
+        var startPos = new Position(0, 0);
+        var endPos = new Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
+
+        console.log(document.lineCount);
+        console.log(document.getText());
+
 	});
-	
+
+    function getLastLineValue(document) {
+        return document.lineCount - 1;
+    }
+
+    function getLastCharacterValue(document) {
+        var lastLine = getLastLineValue(document);
+        return document.lineAt(lastLine).text.length;
+    }
+
+    /**
+     * Replace text in editor
+     *
+    * @param {TextEditor} editor
+    * @param {Range} range
+    * @param {string} newText - new text to replace
+    */
+    function replaceText(editor, range, newText) {
+        editor.edit(function(editBuilder) {
+            editBuilder.replace(range, newText);
+        });
+    }
+
 	context.subscriptions.push(disposable);
 }
 
