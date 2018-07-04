95a38db5767865edba992fa0bff5b8677a1a82ff
Johnny Wong <johnny_temp@yahoo.com.hk>
2018-07-04 18:36:36 +0800

    Improved convert tabs to spaces to the right tabstops

--- src/whitespacer.ts
+++ src/whitespacer.ts
@@ -12,9 +12,31 @@ export class Whitespacer {
      * @return {string} new text after replaced to spaces
      */
     public convertTabsToSpaces(tabSize: number, text: string): string {
-        // use + 1 to add last space
+        /* // use + 1 to add last space
         var spaces = new Array(tabSize + 1).join(' ');
-        var newText = text.replace(/\t/g, spaces);
+        var newText = text.replace(/\t/g, spaces); */
+
+        // convert tabs to the right tabstops property
+        var newText = text.replace(/[^\r\n\t]*\t+/g, function (x) {
+            let width = 0; // characters total width before tab
+            let i = 0;
+            for (; ; ++i) {
+                let c = x.charCodeAt(i);
+                if (c == 0x09) // tab char
+                    break;
+                // simplified way of checking character width
+                if ((c >= 0x0020 && c <= 0x1FFF) || (c >= 0xFF61 && c <= 0xFF9F)) {
+                    width += 1;
+                } else if ((c >= 0x2000 && c <= 0xFF60) || (c >= 0xFFA0)) {
+                    width += 2; // full-width char
+                } else {
+                    width += 0; // zero-width char
+                }
+            }
+            let numTabs = x.length - i;
+            let totalTabSize = tabSize * numTabs - (width % tabSize);
+            return x.substr(0, i) + " ".repeat(totalTabSize);
+        });
 
         return newText;
     }
@@ -27,9 +49,15 @@ export class Whitespacer {
      * @return {string} new text after replaced to tabs
      */
     public convertSpacesToTabs(tabSize: number, text: string): string {
-        // use + 1 to add last space
+        /* // use + 1 to add last space
         var spaces = new Array(tabSize + 1).join(' ');
-        var newText = text.replace(new RegExp(spaces, 'g'), '\t');
+        var newText = text.replace(new RegExp(spaces, 'g'), '\t'); */
+
+        // Only convert leading whitespaces for simplicity.  It is difficult to convert inline spaces to the right tabstop with regular expression.
+        var regex2 = new RegExp("[ ]{" + tabSize + "}|[ ]{0," + (tabSize - 1) + "}\t", 'g');
+        var newText = text.replace(new RegExp("^([ ]{" + tabSize + "}|[ ]{0," + (tabSize - 1) + "}\t)*", 'mg'), function (x) {
+            return x.replace(regex2, '\t');
+        });
 
         return newText;
     }
--- test/extension.test.ts
+++ test/extension.test.ts
@@ -2,29 +2,29 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
-import {Whitespacer} from '../src/whitespacer';
+import { Whitespacer } from '../src/whitespacer';
 
 suite('Extension Tests', () => {
-	test('Whitespacer: Convert tabs to spaces', () => {
-		let whitespacer = new Whitespacer();
+    test('Whitespacer: Convert tabs to spaces', () => {
+        let whitespacer = new Whitespacer();
 
-        var inputText = '\ta\n\t\nb\t\nc\t\td';
+        var inputText = '\ta\n\t\nb\t\nc\t\td\n \te';
         var tabSize = 2;
         var newText = whitespacer.convertTabsToSpaces(tabSize, inputText);
-        assert.equal(newText, '  a\n  \nb  \nc    d');
+        assert.equal(newText, '  a\n  \nb  \nc    d\n  e');
 
         tabSize = 3;
         newText = whitespacer.convertTabsToSpaces(tabSize, inputText);
-        assert.equal(newText, '   a\n   \nb   \nc      d');
-	});
+        assert.equal(newText, '   a\n   \nb   \nc      d\n   e');
+    });
 
     test('Whitespacer: Convert spaces to tabs', () => {
-		let whitespacer = new Whitespacer();
+        let whitespacer = new Whitespacer();
 
-        let inputText = '  a\n  \nb  \nc    d';
+        let inputText = '  a\n  \nb  \nc    d\n   e';
         let tabSize = 2;
         let newText = whitespacer.convertSpacesToTabs(tabSize, inputText);
 
-        assert.equal(newText, '\ta\n\t\nb\t\nc\t\td');
-	});
+        assert.equal(newText, '\ta\n\t\nb\t\nc\t\td\n\t e');
+    });
 });
