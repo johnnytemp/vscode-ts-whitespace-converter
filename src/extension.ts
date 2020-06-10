7ea71a2e74e150e3181031d87e9d8e903cedfb3a
Johnny Wong <johnny_temp@yahoo.com.hk>
2020-06-11 07:21:16 +0800

    Add advanced logic to union overlapping line selections.

--- src/extension.ts
+++ src/extension.ts
@@ -19,10 +19,13 @@ export function activate(context: ExtensionContext) {
         } else {
             let ranges = [];
             let texts = [];
+            let selections = computeLineSelections(document, editor.selections);
+            numSelections = selections.length;
             for (let i: number = 0; i < numSelections; i++) {
-                let sel = editor.selections[i];
+                let sel = selections[i];
                 // window.showInformationMessage('position ' + sel.start.line + ':' + sel.start.character + ' to ' + sel.end.line + ':' + sel.end.character);
-                let range : Range = lineSelectionFromSelection(document, sel);
+                // let range : Range = lineSelectionFromSelection(document, sel);
+                let range : Range = sel;
                 var currentText = document.getText(range);
                 var newText = whitespacer[whitespacerFunctionName](tabSize, currentText);
                 ranges.push(range);
@@ -49,6 +52,34 @@ export function deactivate() {
 function lineSelectionFromSelection(document : TextDocument, sel : Selection) : Selection {
     let endLine = sel.end.line > sel.start.line && sel.end.character == 0 ? sel.end.line - 1 : sel.end.line;
     return new Selection(new Position(sel.start.line, 0), document.lineAt(endLine).range.end);
+}
+
+function computeLineSelections(document, selections) {
+    var lineSelections = [];
+    let numSelections = selections.length;
+    if (numSelections <= 1) {
+        return numSelections == 1 ? [lineSelectionFromSelection(document, selections[0])] : [];
+    }
+    let sortedSelections = selections.slice(0);
+    sortedSelections = sortedSelections.sort((x : Selection, y : Selection) => {
+        let diff = x.start.line - y.start.line;
+        if (diff != 0)
+            return diff;
+        return x.end.line - y.end.line;
+    });
+    let lastSel = lineSelectionFromSelection(document, sortedSelections[0]);
+    lineSelections.push(lastSel);
+    for (let i: number = 1; i < numSelections; i++) {
+        let sel = lineSelectionFromSelection(document, sortedSelections[i]);
+        if (sel.start.line > lastSel.end.line) {   // non-overlapping, no need union
+            lastSel = sel;
+            lineSelections.push(lastSel);
+        } else if (sel.end.line > lastSel.end.line) {    // union and use new line end
+            lineSelections[lineSelections.length - 1] = new Selection(lastSel.start, sel.end);
+        } else {    // discard
+        }
+    }
+    return lineSelections;
 }
 
 /**
@@ -123,6 +154,6 @@ function replaceTexts(editor, ranges, texts) {
         });
     }).then(
         val => {},
-        err => window.showErrorMessage('Tabstop Whitespace Converter: ' + err)  // Show error especially when got "Error: Overlapping ranges are not allowed!" because of two or more selections have same lines.
+        err => window.showErrorMessage('Tabstop Whitespace Converter: ' + err)  // Show error especially when got "Error: Overlapping ranges are not allowed!" because of two or more selections have same lines. (shouldn't happen now)
     );
 }
